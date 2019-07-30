71a3250459377e2eefa074c689d304c74a729aef
Sebastian Riedel <git@basxto.de>
2019-07-31 01:27:47 +0200

    Fix Island Joy 16 colors

--- island_joy_16.h
+++ island_joy_16.h
@@ -1,41 +1,42 @@
 // based on https://lospec.com/palette-list/island-joy-16
 // Island Joy 16 by Kerrie Lake
+// converted with tools/convertcolor.sh
 #ifndef ISLAND_JOY_16_H
 #define ISLAND_JOY_16_H
 
 #include <gb/gb.h>
 #include <gb/cgb.h>
-// /256*32
+
 // #ffffff
 #define IJ16_WHITE (RGB(31, 31, 31))
 // #6df7c1
-#define IJ16_TURQUOISE (RGB(14, 31, 24))
+#define IJ16_TURQUOISE (RGB(13, 30, 23))
 // #11adc1
-#define IJ16_BLUE (RGB(2, 22, 24))
+#define IJ16_BLUE (RGB(2, 21, 23))
 // #606c81
-#define IJ16_GREY (RGB(12, 13, 16))
+#define IJ16_GREY (RGB(11, 13, 15))
 // #393457
-#define IJ16_BLACK (RGB(7, 7, 11))
+#define IJ16_BLACK (RGB(6, 6, 10))
 // #1e8875
-#define IJ16_DARK_GREEN (RGB(4, 17, 15))
+#define IJ16_DARK_GREEN (RGB(3, 16, 14))
 // #5bb361
-#define IJ16_GREEN (RGB(11, 22, 12))
+#define IJ16_GREEN (RGB(11, 21, 11))
 // #a1e55a
-#define IJ16_LIGHT_GREEN (RGB(20, 29, 11))
+#define IJ16_LIGHT_GREEN (RGB(19, 27, 10))
 // #f7e476
-#define IJ16_YELLOW (RGB(31, 29, 15))
+#define IJ16_YELLOW (RGB(30, 27, 14))
 // #f99252
-#define IJ16_ORANGE (RGB(31, 18, 10))
+#define IJ16_ORANGE (RGB(30, 17, 9))
 // #cb4d68
-#define IJ16_RED (RGB(25, 10, 13))
+#define IJ16_RED (RGB(24, 9, 12))
 // #6a3771
-#define IJ16_PURPLE (RGB(13, 7, 14))
+#define IJ16_PURPLE (RGB(12, 6, 13))
 // #c92464
-#define IJ16_DARK_PINK (RGB(25, 5, 13))
+#define IJ16_DARK_PINK (RGB(24, 4, 12))
 // #f48cb6
-#define IJ16_PINK (RGB(31, 18, 13))
+#define IJ16_PINK (RGB(29, 17, 22))
 // #f7b69e
-#define IJ16_LIGHT_PINK (RGB(31, 23, 20))
+#define IJ16_LIGHT_PINK (RGB(30, 22, 19))
 // #9b9c82
-#define IJ16_BEIGE (RGB(19, 20, 16))
+#define IJ16_BEIGE (RGB(18, 18, 15))
 #endif
